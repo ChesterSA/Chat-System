407f3b756eaea0e2fe1100119780cf02da501601
s6089488 <s6089488@linux16.scm.tees.ac.uk>
2018-12-06 13:29:32 +0000

    Cameron's Regex

--- src/peertopeer/Message.java
+++ src/peertopeer/Message.java
@@ -57,22 +57,28 @@ public class Message {
                 String.join(",", to),
                 content);
     }
-    
     static final Pattern MESSAGE_REGEX_PATTERN = Pattern.compile("^FROM:#([A-Za-z]+)#(,TO:#([A-Za-z]*)#)?,CONTENT:#(.+)#$");
+    static final Pattern IP_REGEX_PATTERN = Pattern.compile("^FROM:#([A-Za-z]+)#(,TO:#([A-Za-z]*)#)?,CONTENT:#([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}, *)#$");
 
     public static Message parseMessage(String rawMessage) {
         System.out.println("RAW MESSAGE: " + rawMessage);
         Message newMessage = null;
                 
-        Matcher matcher = MESSAGE_REGEX_PATTERN.matcher(rawMessage);
-
-        if(matcher.matches()) {
-            //System.out.println(matcher.group(1));
-            //System.out.println(matcher.group(3));
-            //System.out.println(matcher.group(4));
-            newMessage = new Message(matcher.group(1),matcher.group(3));
-            newMessage.append(matcher.group(4));
+        Matcher matcher1 = MESSAGE_REGEX_PATTERN.matcher(rawMessage);
+        Matcher matcher2 = IP_REGEX_PATTERN.matcher(rawMessage);
+        
+        if(matcher1.matches()) {
+                    if (matcher2.matches()) {
+            System.out.println("I have alot of Ips");
         }
+            //System.out.println(matcher1.group(1));
+            //System.out.println(matcher1.group(3));
+            //System.out.println(matcher1.group(4));
+            newMessage = new Message(matcher1.group(1),matcher1.group(3));
+            newMessage.append(matcher1.group(4));
+        }
+        
+
         //append the content
         return newMessage;
     }
