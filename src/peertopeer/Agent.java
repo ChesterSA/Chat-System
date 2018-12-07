90434f466b043582d3103d344c1fb761c2caadda
s6089488 <s6089488@linux16.scm.tees.ac.uk>
2018-12-07 14:40:47 +0000

    Agent and Portal Driver classes added

--- src/peertopeer/Agent.java
+++ src/peertopeer/Agent.java
@@ -5,7 +5,16 @@
  */
 package peertopeer;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -52,6 +61,102 @@ public class Agent extends ChatNode {
                 }
             }
         }
+    }
+    
+    @Override
+    public void connectTo(final String remoteIpAddress, final int remotePort)
+    {
+        // check if we're already connected, perhaps the remote device
+        // instigated a connection previously.
+        if (isalreadyConnected(remoteIpAddress))
+        {
+            //System.err.println(String.format("Already connected to the peer with IP: '%s'", remoteIpAddress));
+            return;
+        }
+
+        //Create a thread to instigate the HELLO handshake between this peer
+        //and the remote peer
+        Thread helloAgentThread = new Thread(
+                new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                InetAddress bindAddress;
+                try
+                {
+                    bindAddress = InetAddress.getByName(remoteIpAddress);
+                    Socket newSocket = new Socket(bindAddress, remotePort);
+                    Connection partialConnection = new Connection(newSocket);
+                    partialConnection.sendMessage(Message.createAgentMessage(handle));
+
+                    //Wait for a response from this connection.
+                    while (!partialConnection.hasMessage())
+                    {
+                        // ... Do nothing ...
+                        // assumes it will eventually connect... probably not a good idea...
+                    }
+
+                    //We should have a HELLOACK message, which will have
+                    //the handle of the remote peer
+                    final Message receivedMessage = partialConnection.receiveMessage();
+                    //Message ackMessage = partialConnection.receiveMessage();
+
+                    if (receivedMessage.isHelloAckMessage())
+                    {
+                        partialConnection.setHandle(receivedMessage.getFrom());
+                        addConnection(partialConnection);
+                    }
+                    else if (receivedMessage.isDirMessage())
+                    {
+                        String[] ips = receivedMessage.getContent().split(",");
+
+                        for (String ip : ips)
+                        {
+                            boolean newConnection = true;
+
+                            if (ip.equals(Inet4Address.getLocalHost().getHostAddress()))
+                            {
+                                newConnection = false;
+                            }
+                            else
+                            {
+                                
+                                for (Connection c : peerGroupConnections.values())
+                                {
+                                    Pattern ipPattern = Pattern.compile("(?<=/)(.*?)(?=,)");
+                                    
+                                    Matcher m = ipPattern.matcher(c.socket.toString());                                   
+                                    
+                                    if (m.group().equals(ip))
+                                    {
+                                        newConnection = false;
+                                    }
+                                    System.out.println(m.group() + "    " + newConnection);
+                                }
+                            }
+                            if (newConnection)
+                            {
+                                connectTo(ip);
+                            }
+                        }
+                    }
+                }
+                catch (UnknownHostException ex)
+                {
+                    Logger.getLogger(ChatNode.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                catch (IOException ex)
+                {
+                    Logger.getLogger(ChatNode.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }
+        );
+
+        helloAgentThread.start();
+
     }
     
 }
