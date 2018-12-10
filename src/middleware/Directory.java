4573e94f1fcb3c5e2e65b9831312dd1b13a237c5
s6089488 <s6089488@TU99758.windows.tees.ac.uk>
2018-12-10 16:09:19 +0000

    Directory now uses ip regex

--- src/middleware/Directory.java
+++ src/middleware/Directory.java
@@ -15,51 +15,51 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author s6089488
  */
-public class Directory extends ChatNode
-{
+public class Directory extends ChatNode {
 
     //Messages are sent as a client.
     //
-
     /**
      * The list of connections the directory currently holds.
-     */ 
+     */
     protected HashMap<String, Connection> connections = new HashMap<>();
 
     /**
      * Constructor to give the directory an identifying string.
+     *
      * @param handle Used to pair it with its connection around the system.
-     */ 
-    public Directory(String handle)
-    {
+     */
+    public Directory(String handle) {
         super(handle, DEFAULT_RECV_IP_ADDRESS, DEFAULT_PORT);
     }
 
     /**
-     * Constructor with string handle, along with the IP string of its connection.
+     * Constructor with string handle, along with the IP string of its
+     * connection.
+     *
      * @param handle Used to pair it with its connection around the system.
-     * @param receiveIp 
-     */ 
-    public Directory(String handle, String receiveIp)
-    {
+     * @param receiveIp
+     */
+    public Directory(String handle, String receiveIp) {
         super(handle, receiveIp, DEFAULT_PORT);
     }
 
     /**
-     * Constructor with string handle, string recieveIp, and the integer
-     * port number.
-     * of the directory.
+     * Constructor with string handle, string recieveIp, and the integer port
+     * number. of the directory.
+     *
      * @param handle Used to identify the connection around the system.
      * @param receiveIp Used to locate the directory around the system.
      * @param receivePort Used to locate the port of the connection.
      */
-    public Directory(String handle, String receiveIp, int receivePort)
-    {
+    public Directory(String handle, String receiveIp, int receivePort) {
         super(handle, receiveIp, receivePort);
     }
 
@@ -67,8 +67,7 @@ public class Directory extends ChatNode
      * Removes all current connections from the directory.
      */
     @Override
-    public void removeConnections()
-    {
+    public void removeConnections() {
         connections = new HashMap<>();
     }
 
@@ -76,15 +75,11 @@ public class Directory extends ChatNode
      * Thread which will accept messages from other connections in the system.
      */
     protected Thread acceptThread = new Thread(
-            new Runnable()
-    {
+            new Runnable() {
         @Override
-        public void run()
-        {
-            while (true)
-            {
-                try
-                {
+        public void run() {
+            while (true) {
+                try {
                     final Socket newClientSocket = serverSocket.accept();
 
                     //Create a partial connection
@@ -92,8 +87,7 @@ public class Directory extends ChatNode
 
                     System.out.println("Awaiting PORTAL message from new connection");
 
-                    while (!newConnection.hasMessage())
-                    {
+                    while (!newConnection.hasMessage()) {
                         // wait for a message from the new connection...
                         // should probably handle timeouts...
                     }
@@ -105,21 +99,15 @@ public class Directory extends ChatNode
 
                     System.out.println("Message received: " + receivedMessage.toString());
 
-                    if (!(receivedMessage.getType().equals(MessageType.PORTAL)))
-                    {
+                    if (!(receivedMessage.getType().equals(MessageType.PORTAL))) {
                         System.err.println("Invalid Portal connect message, connection attempt will be dropped.");
-                    }
-                    else
-                    {
+                    } else {
                         final String newConnectionHandle = receivedMessage.getFrom();
 
-                        if (newConnectionHandle != null)
-                        {
-                            synchronized (lock)
-                            {
+                        if (newConnectionHandle != null) {
+                            synchronized (lock) {
 
-                                if (connections.get(newConnectionHandle) == null)
-                                {
+                                if (connections.get(newConnectionHandle) == null) {
                                     //Complete the connection by setting its handle.
                                     //this is essential as we use the handle to send
                                     //messages to our peers.
@@ -133,27 +121,22 @@ public class Directory extends ChatNode
                                     //The HELLOACK allows the peer to know our handle
                                     //
                                     newConnection.sendMessage(createDirMessage(handle, newConnectionHandle));
-                                }
-                                else
-                                {
+                                } else {
                                     connections.remove(newConnectionHandle);
                                     newConnection.setHandle(newConnectionHandle);
                                     addConnection(newConnection);
-                                    for (Connection c : connections.values())
-                                    {
+                                    for (Connection c : connections.values()) {
                                         c.sendMessage(createDirMessage(handle, c.getHandle()));
                                     }
                                     //newConnection.sendMessage(createDirMessage(handle, newConnectionHandle));
-                                    
+
                                 }
                             }
                         }
                     }
                     // Check for HELLO message with client name.
 
-                }
-                catch (IOException ex)
-                {
+                } catch (IOException ex) {
                     Logger.getLogger(ChatNode.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
@@ -164,19 +147,23 @@ public class Directory extends ChatNode
 
     /**
      * Creates message object with current directory connections.
+     *
      * @param from The directory sending the message.
      * @param to The message reciever.
      * @return Single string of all directory's current IP addresses.
      */
-    private Message createDirMessage(String from, String to)
-    {
+    private Message createDirMessage(String from, String to) {
         Message m = new Message(from, to, MessageType.DIR);
         String content = "";
-        for (Connection c : connections.values())
-        {
-            System.out.println(c.socket.toString().substring(13, 27));
+        for (Connection c : connections.values()) {
+            Pattern ipPattern = Pattern.compile("(?<=\\/)(.*?)(?=,)");
+            System.out.println(c.socket.toString());
+            Matcher match = ipPattern.matcher(c.socket.toString());
 
-            content += c.socket.toString().substring(13, 27) + ",";
+            System.out.println("found " + match.group());
+
+            content += match.group();
+
         }
         m.append(content);
         return m;
@@ -184,45 +171,44 @@ public class Directory extends ChatNode
 
     /**
      * Starts the directory's ability to recieve new peer connections.
+     *
      * @throws IOException Handles errors with input/output errors.
      */
     @Override
-    public void begin() throws IOException
-    {
+    public void begin() throws IOException {
         startPeerReceiver();
     }
 
     /**
      * Checks if directory holds any current connections.
+     *
      * @return If size of connection list is more than 0
      */
-    public synchronized boolean hasConnections()
-    {
+    public synchronized boolean hasConnections() {
         return connections.size() > 0;
     }
 
     /**
      * Returns list of all identifying strings inside connection list.
+     *
      * @return LinkedList which holds all keys inside connections list.
      */
-    public synchronized List<String> getConnectionHandles()
-    {
+    public synchronized List<String> getConnectionHandles() {
         List<String> handles = new LinkedList<>();
         handles.addAll(connections.keySet());
         return handles;
     }
 
     /**
-     * Starts directory's ability to recieve new peer connections by starting
-     * an instance of Thread acceptThread.
+     * Starts directory's ability to recieve new peer connections by starting an
+     * instance of Thread acceptThread.
+     *
      * @throws UnknownHostException Handles errors of unknown IP addresses.
      * @throws IOException Handles errors with input/output errors.
      */
     @Override
-    protected void startPeerReceiver() throws UnknownHostException, IOException
-    {
-        if (serverSocket == null)
-        {
+    protected void startPeerReceiver() throws UnknownHostException, IOException {
+        if (serverSocket == null) {
             InetAddress bindAddress = InetAddress.getByName(this.receiveIp);
             serverSocket = new ServerSocket(this.receivePort, 0, bindAddress);
             acceptThread.start();
@@ -231,14 +217,12 @@ public class Directory extends ChatNode
 
     /**
      * Adds new connection to the directory's current connection list.
+     *
      * @param connection Connection to be added.
      */
-    protected void addConnection(final Connection connection)
-    {
-        synchronized (lock)
-        {
-            if (connections.containsKey(connection.getHandle()))
-            {
+    protected void addConnection(final Connection connection) {
+        synchronized (lock) {
+            if (connections.containsKey(connection.getHandle())) {
                 System.err.println("[" + connection.getHandle() + "] is already an established connection.");
                 return;
             }
@@ -248,16 +232,14 @@ public class Directory extends ChatNode
 
     /**
      * Checks if directory is already connected to an IP address.
+     *
      * @param ipAddress IP to be checked.
      * @return If IP address is found in connections list.
      */
     @Override
-    protected synchronized boolean isalreadyConnected(final String ipAddress)
-    {
-        for (Connection c : connections.values())
-        {
-            if (c.hasIpAddress(ipAddress))
-            {
+    protected synchronized boolean isalreadyConnected(final String ipAddress) {
+        for (Connection c : connections.values()) {
+            if (c.hasIpAddress(ipAddress)) {
                 return true;
             }
         }
@@ -266,33 +248,25 @@ public class Directory extends ChatNode
 
     /**
      * Sends message to a recipient.
+     *
      * @param message Message to be sent.
      */
-
     @Override
-    public void sendMessage(Message message)
-    {
-        synchronized (lock)
-        {
-            if (message.getType().equals(MessageType.BROADCAST))
-            {
+    public void sendMessage(Message message) {
+        synchronized (lock) {
+            if (message.getType().equals(MessageType.BROADCAST)) {
                 //
                 // Not handling broadcast messages presently...
                 //
-            }
-            else
-            {
+            } else {
                 final String receiver = message.getTo();
 
                 //find the socket of the peer using their handle:
                 Connection peerConnection = connections.get(receiver);
 
-                if (peerConnection != null)
-                {
+                if (peerConnection != null) {
                     peerConnection.sendMessage(message);
-                }
-                else
-                {
+                } else {
                     System.err.println("'" + receiver + "' is an unknown peer");
                 }
 
@@ -302,13 +276,12 @@ public class Directory extends ChatNode
 
     /**
      * Gets all IP addresses in current connections list.
+     *
      * @return String which contains all stored IPs.
      */
-    public String getAddresses()
-    {
+    public String getAddresses() {
         String output = "";
-        for (Connection c : connections.values())
-        {
+        for (Connection c : connections.values()) {
             System.out.println(c.socket.toString().substring(13, 27));
 
             output += c.socket.toString().substring(13, 27) + ",";
