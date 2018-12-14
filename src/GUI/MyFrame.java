736de70d198c302ed774c43f2cb117ec879e9679
s6089488 <s6089488@TU69591.windows.tees.ac.uk>
2018-12-14 12:45:59 +0000

    Moved Agent and Portal ip and handle checks from GUI to chatnode

--- src/GUI/MyFrame.java
+++ src/GUI/MyFrame.java
@@ -9,12 +9,11 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import middleware.ChatNode;
 import middleware.Contactable;
 import middleware.Message;
 
@@ -24,15 +23,15 @@ import middleware.Message;
  */
 public abstract class MyFrame extends JFrame implements Contactable
 {
+
     final Insets INSETS_DATA = new Insets(2, 2, 2, 2);
     final String IP_BASE = "152.105.67.";
-    
 
     public MyFrame(String title)
     {
         super(title);
     }
-   
+
     /**
      * JOptionPane message that asks for the ip
      *
@@ -40,40 +39,27 @@ public abstract class MyFrame extends JFrame implements Contactable
      */
     protected String getIpAddress()
     {
-        //Checks for 4 groups of numbers between 1 and 255
-        Pattern ipPattern = Pattern.compile("\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
-                                             + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
-                                             + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
-                                             + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b");
-        boolean find = false;
-
         String ip = "";
         do
         {
             ip = JOptionPane.showInputDialog("Enter IP Address", IP_BASE);
-            Matcher matcher = ipPattern.matcher(ip);
-
-            if (matcher.matches())
-            {
-                find = true;
-            }
-        } while (!find);
+        } while (!ChatNode.CheckIp(ip));
 
         return ip;
     }
-    
+
     protected void addComponentToGridBag(Container container, Component component, int gridx, int gridy, int gridwidth, int gridheight, int anchor, int fill)
     {
         GridBagConstraints gridBagConstraints = new GridBagConstraints(gridx, gridy, gridwidth, gridheight, 1.0, 1.0, anchor, fill, INSETS_DATA, 0, 0);
         container.add(component, gridBagConstraints);
     }
-    
+
     protected String getContent(String to)
     {
         String content = JOptionPane.showInputDialog(null, "What message would you like to send to " + to, "Send Message");
         return content;
     }
-    
+
     /**
      * Gets input for a ChatNode's handle.
      *
@@ -83,29 +69,26 @@ public abstract class MyFrame extends JFrame implements Contactable
     {
         String handle = "";
 
-        while (handle.isEmpty())
+        while (!ChatNode.CheckHandle(handle))
         {
             handle = JOptionPane.showInputDialog(null, "Enter handle", "Handle", JOptionPane.QUESTION_MESSAGE);
             if (handle == null)
             {
                 setVisible(false);
-                
+
                 //Doesn't error now, but immediately exiting program isn't useful
                 System.exit(0);
             }
-            if (!handle.matches("^[^\\d\\s]+$"))
-            {
-                handle = "";
-            }
         }
-        
+
         dispose();
 
         return handle;
     }
-    
-     /**
+
+    /**
      * Check the message information and react accordingly
+     *
      * @param m the message being handled
      */
     @Override
@@ -124,23 +107,25 @@ public abstract class MyFrame extends JFrame implements Contactable
                 "Message Notification",
                 JOptionPane.INFORMATION_MESSAGE);
     }
-    
+
     @Override
     public void connectTo(String ip)
     {
-        
+
     }
-    
+
     /**
      * Send a message from this class to any other agent on the network
+     *
      * @param to the handle of the agent being sent to
      * @param content the content of the message to send
      */
+    @Override
     public void sendMessage(String to, String content)
     {
-        
+
     }
-    
+
     protected abstract void addButtons();
-    
+
 }
--- src/middleware/ChatNode.java
+++ src/middleware/ChatNode.java
@@ -8,12 +8,17 @@ package middleware;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * The abstract extended by all classes that need to communicate over the network
+ * The abstract extended by all classes that need to communicate over the
+ * network
+ *
  * @author Group B
  */
-public abstract class ChatNode {
+public abstract class ChatNode
+{
 
     /**
      * The lock used by synchronised threads
@@ -52,28 +57,34 @@ public abstract class ChatNode {
 
     /**
      * Creates a new ChatNode from the specified handle
+     *
      * @param handle the unique handle of the chatnode
      */
-    public ChatNode(String handle) {
+    public ChatNode(String handle)
+    {
         this(handle, DEFAULT_RECV_IP_ADDRESS, DEFAULT_PORT);
     }
 
     /**
      * Creates a new ChatNode from the specified handle and IP
+     *
      * @param handle the unique handle of the chatnode
      * @param receiveIp the ip that this node can receive requests from
      */
-    public ChatNode(String handle, String receiveIp) {
+    public ChatNode(String handle, String receiveIp)
+    {
         this(handle, receiveIp, DEFAULT_PORT);
     }
 
     /**
      * Creates a new ChatNode from the specified handle, IP, and port
+     *
      * @param handle the unique handle of the chatnode
      * @param receiveIp the ip that this node can receive requests from
      * @param receivePort the port that this node can receive requests from
      */
-    public ChatNode(String handle, String receiveIp, int receivePort) {
+    public ChatNode(String handle, String receiveIp, int receivePort)
+    {
         this.handle = handle;
         this.receiveIp = receiveIp;
         this.receivePort = receivePort;
@@ -81,42 +92,64 @@ public abstract class ChatNode {
 
     /**
      * abstract method to start the nodes running so they can receive messages
+     *
      * @throws IOException
      */
     abstract public void begin() throws IOException;
 
     /**
      * Open this node to receive new connections from the network
+     *
      * @throws UnknownHostException
      * @throws IOException
      */
     protected abstract void startPeerReceiver() throws UnknownHostException, IOException;
-    
+
     /**
      * cannot declare abstract, but needs to be overriden
+     *
      * @param ipAddress the ip address to be checked
      * @return
      */
-    protected synchronized boolean isalreadyConnected(final String ipAddress) {
+    protected synchronized boolean isalreadyConnected(final String ipAddress)
+    {
         return false;
     }
-   
+
     /**
      * Gets the handle of the node
+     *
      * @return the handle of the node
      */
-    public String getHandle() {
+    public String getHandle()
+    {
         return handle;
     }
 
     /**
      * Abstract method to send a message
+     *
      * @param message the message to be sent
      */
     //public abstract void sendMessage(Message message);
-    
     /**
      * abstract method to remove connections from the node
      */
     public abstract void removeConnections();
+
+    public static boolean CheckHandle(String handle)
+    {
+        return handle.matches("^[^\\d\\s]+$");
+    }
+
+    public static boolean CheckIp(String ip)
+    {
+        Pattern ipPattern = Pattern.compile("\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
+                + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
+                + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
+                + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b");
+        Matcher matcher = ipPattern.matcher(ip);
+
+        return matcher.matches();
+    }
 }
